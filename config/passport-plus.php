73a0d0f97592a255d51d8d9526ce7fb42adb0992
mnikoei <mohsen.nikoei@gmail.com>
2021-06-06 15:57:40 +0430

    comments for config options

--- config/passport-plus.php
+++ config/passport-plus.php
@@ -2,8 +2,26 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Caching data
+    |--------------------------------------------------------------------------
+    |
+    | This option determines if user and token data should be read from
+    | cache or not, if set true it uses customized version of passport
+    | token guard and repositories that are cache based
+    |
+    */
     'cache' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cache TTL
+    |--------------------------------------------------------------------------
+    |
+    | This options defines how long data should be kept in cache
+    |
+    */
     'cache-ttl' => 60
 
 ];
